59e4b96dfaaa0be96b233e980e8cc7cbd4fe7b89
huylebron <doquochuy27102003@gmail.com>
2024-09-28 22:55:11 +0700

    config connection String

--- HuyLebron/Program.cs
+++ HuyLebron/Program.cs
@@ -1,9 +1,17 @@
+using HuyLebron.Web.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 
 var app = builder.Build();
+
+app.Run();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
