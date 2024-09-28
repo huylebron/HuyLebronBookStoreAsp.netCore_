e05f80e41f865162d995660f5e051f22993df7b8
huylebron <doquochuy27102003@gmail.com>
2024-09-28 22:54:32 +0700

    create DbContext using EF core

---
+++ HuyLebron/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using HuyLebron.Web.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HuyLebron.Web.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
